8ef19f7ca93345627603d8e587d0bc5f0af39a65
Superxiang <xiang0911@gmail.com>
2018-09-04 16:36:21 +0800

    first add
    first add

---
+++ ex1.py
@@ -0,0 +1,5 @@
+print("Hello world!")
+print('Hello world, again!')
+print("What's your name, boy?")
+print('My name is "super"')
+print("Nice to meet you, 'super'")
---
+++ ex5.py
@@ -0,0 +1,7 @@
+my_name = 'super xiang'
+my_age = 35  # really
+my_height = 172  # cm
+my_weight = 70  #kg
+
+print(f"The boy's name is {my_name}, ")
+print(f"he is {my_age} years old, {my_height} cm height, and {my_weight} kgs weight.")
