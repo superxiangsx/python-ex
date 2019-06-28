4e8f3e65652742f3c4daee70327037506704ae2e
Superxiang <xiang0911@gmail.com>
2019-06-28 13:33:13 +0800

    some small test
    just some small test

--- ex11.py
+++ ex11.py
@@ -1,7 +1,6 @@
 from sys import argv
 #read the WYSS section for how to run this
 script, first, second, third = argv
-
 print(f"The script is called: {script}")
 print(f"The first is called: {first}")
 print(f"The second is called: {second}")
